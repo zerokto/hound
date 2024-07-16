365cb3c4abd6e25025832d52e706715306faa5a6
keto <ahwd0213@163.com>
2024-07-16 23:44:44 +0800

    chore: index/read.go

--- codesearch/index/read.go
+++ codesearch/index/read.go
@@ -94,19 +94,23 @@ type Index struct {
 const postEntrySize = 3 + 4 + 4
 
 func Open(file string) *Index {
-	mm := mmap(file)
+	mm := mmap(file) // 将文件映射到内存
+
+	// 长度不满足条件，文件结尾不符合要求
 	if len(mm.d) < 4*4+len(trailerMagic) || string(mm.d[len(mm.d)-len(trailerMagic):]) != trailerMagic {
 		corrupt(mm.f)
 	}
+
+	// 计算从数据开始到 trailerMagic 之前的字节数
 	n := uint32(len(mm.d) - len(trailerMagic) - 5*4)
 	ix := &Index{data: mm}
-	ix.pathData = ix.uint32(n)
+	ix.pathData = ix.uint32(n) // 通过文件尾部的偏移量获取pathData
 	ix.nameData = ix.uint32(n + 4)
 	ix.postData = ix.uint32(n + 8)
 	ix.nameIndex = ix.uint32(n + 12)
 	ix.postIndex = ix.uint32(n + 16)
-	ix.numName = int((ix.postIndex-ix.nameIndex)/4) - 1
-	ix.numPost = int((n - ix.postIndex) / postEntrySize)
+	ix.numName = int((ix.postIndex-ix.nameIndex)/4) - 1  // 计算名称索引中条目的数量  再减去1（因为最后一个条目是结束标记)
+	ix.numPost = int((n - ix.postIndex) / postEntrySize) // 计算倒排列表索引中条目的数量
 	return ix
 }
 
@@ -114,12 +118,13 @@ func Open(file string) *Index {
 // If n >= 0, the slice must have length at least n and is truncated to length n.
 func (ix *Index) slice(off uint32, n int) []byte {
 	o := int(off)
-	if uint32(o) != off || n >= 0 && o+n > len(ix.data.d) {
+	if uint32(o) != off || n >= 0 && o+n > len(ix.data.d) { // 判断是否越界
 		corrupt(ix.data.f)
 	}
 	if n < 0 {
 		return ix.data.d[o:]
 	}
+
 	return ix.data.d[o : o+n]
 }
 
@@ -134,7 +139,7 @@ func (ix *Index) Close() error {
 
 // uvarint returns the varint value at the given offset in the index data.
 func (ix *Index) uvarint(off uint32) uint32 { //nolint
-	v, n := binary.Uvarint(ix.slice(off, -1))
+	v, n := binary.Uvarint(ix.slice(off, -1)) // 读取到末尾
 	if n <= 0 {
 		corrupt(ix.data.f)
 	}
@@ -146,29 +151,29 @@ func (ix *Index) Paths() []string {
 	off := ix.pathData
 	var x []string
 	for {
-		s := ix.str(off)
+		s := ix.str(off) // 读取字符串
 		if len(s) == 0 {
 			break
 		}
 		x = append(x, string(s))
-		off += uint32(len(s) + 1)
+		off += uint32(len(s) + 1) // 添加偏移量
 	}
 	return x
 }
 
 // NameBytes returns the name corresponding to the given fileid.
 func (ix *Index) NameBytes(fileid uint32) []byte {
-	off := ix.uint32(ix.nameIndex + 4*fileid)
+	off := ix.uint32(ix.nameIndex + 4*fileid) // 计算文件id对应的偏移量
 	return ix.str(ix.nameData + off)
 }
 
 func (ix *Index) str(off uint32) []byte {
 	str := ix.slice(off, -1)
-	i := bytes.IndexByte(str, '\x00')
+	i := bytes.IndexByte(str, '\x00') // 查找字符串结束标记
 	if i < 0 {
 		corrupt(ix.data.f)
 	}
-	return str[:i]
+	return str[:i] // 返回字符串
 }
 
 // Name returns the name corresponding to the given fileid.
@@ -178,24 +183,25 @@ func (ix *Index) Name(fileid uint32) string {
 
 // listAt returns the index list entry at the given offset.
 func (ix *Index) listAt(off uint32) (trigram, count, offset uint32) {
-	d := ix.slice(ix.postIndex+off, postEntrySize)
-	trigram = uint32(d[0])<<16 | uint32(d[1])<<8 | uint32(d[2])
-	count = binary.BigEndian.Uint32(d[3:])
-	offset = binary.BigEndian.Uint32(d[3+4:])
+	d := ix.slice(ix.postIndex+off, postEntrySize)              // 从倒排列表索引的起始位置 ix.postIndex 加上偏移量 off 开始，读取一个长度为 postEntrySize 的字节切片
+	trigram = uint32(d[0])<<16 | uint32(d[1])<<8 | uint32(d[2]) // 三个字节合并成一个32位的整数
+	count = binary.BigEndian.Uint32(d[3:])                      // 解析文件数量
+	offset = binary.BigEndian.Uint32(d[3+4:])                   // 从切片 d 的第七个字节开始读取一个32位的大端序整数，得到倒排列表的偏移量
 	return
 }
 
 func (ix *Index) dumpPosting() { //nolint
-	d := ix.slice(ix.postIndex, postEntrySize*ix.numPost)
+	d := ix.slice(ix.postIndex, postEntrySize*ix.numPost) // 读取倒排列表索引的起始位置到末尾，每个条目 postEntrySize 个字节
 	for i := 0; i < ix.numPost; i++ {
 		j := i * postEntrySize
 		t := uint32(d[j])<<16 | uint32(d[j+1])<<8 | uint32(d[j+2])
 		count := int(binary.BigEndian.Uint32(d[j+3:]))
 		offset := binary.BigEndian.Uint32(d[j+3+4:])
-		log.Printf("%#x: %d at %d", t, count, offset)
+		log.Printf("%#x: %d at %d", t, count, offset) // 用于打印索引中所有倒排列表条目的详细信息
 	}
 }
 
+// 通过二分搜索在倒排列表索引中查找与给定trigram相匹配的倒排列表条目，返回条目的数量和偏移量。
 func (ix *Index) findList(trigram uint32) (count int, offset uint32) {
 	// binary search
 	d := ix.slice(ix.postIndex, postEntrySize*ix.numPost)
@@ -218,12 +224,12 @@ func (ix *Index) findList(trigram uint32) (count int, offset uint32) {
 }
 
 type postReader struct {
-	ix       *Index
-	count    int
-	offset   uint32
-	fileid   uint32
-	d        []byte
-	restrict []uint32
+	ix       *Index   // 索引对象的引用
+	count    int      // 倒排列表中的文件数量
+	offset   uint32   // 倒排列表的偏移量
+	fileid   uint32   // 当前文件ID
+	d        []byte   // 倒排列表的剩余数据
+	restrict []uint32 // 限制搜索的文件ID列表
 }
 
 func (r *postReader) init(ix *Index, trigram uint32, restrict []uint32) {
@@ -246,30 +252,31 @@ func (r *postReader) max() int {
 func (r *postReader) next() bool {
 	for r.count > 0 {
 		r.count--
-		delta64, n := binary.Uvarint(r.d)
+		delta64, n := binary.Uvarint(r.d) // 读取倒排列表中的文件ID，返回文件ID和读取的字节数
 		delta := uint32(delta64)
 		if n <= 0 || delta == 0 {
 			corrupt(r.ix.data.f)
 		}
-		r.d = r.d[n:]
-		r.fileid += delta
+
+		r.d = r.d[n:]     // 更新倒排列表的剩余数据
+		r.fileid += delta // 累加文件ID
 		if r.restrict != nil {
 			i := 0
 			for i < len(r.restrict) && r.restrict[i] < r.fileid {
 				i++
 			}
-			r.restrict = r.restrict[i:]
-			if len(r.restrict) == 0 || r.restrict[0] != r.fileid {
+			r.restrict = r.restrict[i:]                            // 更新限制搜索的文件ID列表
+			if len(r.restrict) == 0 || r.restrict[0] != r.fileid { // 当前文件ID不在限制列表中，跳过
 				continue
 			}
 		}
 		return true
 	}
 	// list should end with terminating 0 delta
-	if r.d != nil && (len(r.d) == 0 || r.d[0] != 0) {
+	if r.d != nil && (len(r.d) == 0 || r.d[0] != 0) { // 倒排列表的剩余数据不以0结尾，则说明索引文件格式错误
 		corrupt(r.ix.data.f)
 	}
-	r.fileid = ^uint32(0)
+	r.fileid = ^uint32(0) // 标记当前文件ID无效
 	return false
 }
 
@@ -291,6 +298,7 @@ func (ix *Index) PostingAnd(list []uint32, trigram uint32) []uint32 {
 	return ix.postingAnd(list, trigram, nil)
 }
 
+// 返回list中与trigram匹配的文件ID列表, 求交集
 func (ix *Index) postingAnd(list []uint32, trigram uint32, restrict []uint32) []uint32 {
 	var r postReader
 	r.init(ix, trigram, restrict)
@@ -298,10 +306,11 @@ func (ix *Index) postingAnd(list []uint32, trigram uint32, restrict []uint32) []
 	i := 0
 	for r.next() {
 		fileid := r.fileid
-		for i < len(list) && list[i] < fileid {
+		for i < len(list) && list[i] < fileid { // 跳过list中比fileid小的文件ID
 			i++
 		}
-		if i < len(list) && list[i] == fileid {
+
+		if i < len(list) && list[i] == fileid { // 找到list中等于fileid的文件ID
 			x = append(x, fileid)
 			i++
 		}
@@ -313,6 +322,7 @@ func (ix *Index) PostingOr(list []uint32, trigram uint32) []uint32 {
 	return ix.postingOr(list, trigram, nil)
 }
 
+// 返回list中与trigram匹配的文件ID列表, 求并集
 func (ix *Index) postingOr(list []uint32, trigram uint32, restrict []uint32) []uint32 {
 	var r postReader
 	r.init(ix, trigram, restrict)
@@ -337,6 +347,7 @@ func (ix *Index) PostingQuery(q *Query) []uint32 {
 	return ix.postingQuery(q, nil)
 }
 
+// 用于处理查询 q 并返回与查询匹配的文件ID列表
 func (ix *Index) postingQuery(q *Query, restrict []uint32) (ret []uint32) {
 	var list []uint32
 	switch q.Op {
@@ -389,6 +400,7 @@ func (ix *Index) postingQuery(q *Query, restrict []uint32) (ret []uint32) {
 	return list
 }
 
+// 合并两个有序的列表
 func mergeOr(l1, l2 []uint32) []uint32 {
 	var l []uint32
 	i := 0
